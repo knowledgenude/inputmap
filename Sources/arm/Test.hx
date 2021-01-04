b4d1b73d621639fa906b05da12440678df371566
knowledgenude <phenriquefel@gmail.com>
2021-01-04 11:38:53 -0300

    add-more-examples

--- Sources/arm/Test.hx
+++ Sources/arm/Test.hx
@@ -18,9 +18,17 @@ class Test extends iron.Trait {
 			input = new InputMap(0);
 			input.setCurrentTag("Keyboard");
 			input.createAxis("move", 0.0, 10); // name, pressure, deadzone
+			input.createAction("action"); // optional params: pressure, deadzone
+
+			var action = input.getAction("action");
+			action.addKeyboardComponent("Keyboard", "w", ["control"]);
+			action.addKeyboardComponent("Keybiard", "up", ["alt"]);
+			action.addKeyboardComponent("Keyboard", "a");
 
 			var move = input.getAxis("move");
 			move.addMouseComponent("x", "Keyboard", "movement x", "movement x"); // position, tag, positive key, negative key
+			move.addKeyboardComponent("x", "Keyboard", "d", "a");
+			move.addKeyboardComponent("y", "Keyboard", "w", "s");
 			move.setScale(5.0, 5.0);
 			//move.enableNormalize(); // don't use normalize for values outside 0-1 range like mouse movement
 		});
@@ -28,6 +36,9 @@ class Test extends iron.Trait {
 		notifyOnUpdate(function() {
 			var moveVec = input.getVec("move");
 			rb.setLinearVelocity(moveVec.x, moveVec.y, moveVec.z);
+
+			if (input.isActionPressed("action")) trace("action pressed");
+			if (input.isActionReleased("action")) trace("action released");
 		});
 	}
 }
